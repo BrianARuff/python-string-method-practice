8f6954877217a932fe21899b1127c19d794bb957
BrianARuff <thelonewolf198927@gmail.com>
2018-11-03 14:30:19 -0400

    compare two strings and print if they match or not

---
+++ 02_string_methods.py
@@ -0,0 +1,10 @@
+# Define new string and store in variable.
+string = "Hello, world."
+
+# Define another string that contains all of string one and more.
+string_two = "Hello, world. There is light."
+
+# Compare string 2 against string 1 and see if string 1 contains string 2.
+
+s2_in_s1 = string_two in string
+print(s2_in_s1)
