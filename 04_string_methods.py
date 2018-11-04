932e3d0c0315e742b56a23d31829340f5c455a6a
BrianARuff <thelonewolf198927@gmail.com>
2018-11-03 21:05:55 -0400

    right justify padding string

--- 04_string_methods.py
+++ 04_string_methods.py
@@ -6,6 +6,10 @@ print(string)
 stripped_string = string.strip()
 print(stripped_string)
 
-# Justify string with the total amount of white space in string and then print it out.
+# Justify string on left side with the total amount of white space in string and then print it out.
 l_just_string = stripped_string.ljust(len(string), '.')
 print(l_just_string)
+
+# Justify string on right side with the total amount of white space in string and then print it out.
+r_just_string = stripped_string.rjust(len(string), '.')
+print(r_just_string)
