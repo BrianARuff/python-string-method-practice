6620dfec19fc13aa428f266f41c6078491f7a3cb
BrianARuff <thelonewolf198927@gmail.com>
2018-11-03 20:14:36 -0400

    count subtring occurences in main string

---
+++ 03_string_methods.py
@@ -0,0 +1,9 @@
+# Define initial string.
+string = "Happy, Happy Birthday!!"
+
+# Define second string.
+string_two = "Happy"
+
+# Count occurence of string_two in string and then print it.
+occurence = string.count(string_two)
+print(occurence);
