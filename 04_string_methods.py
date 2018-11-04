29c42663bfcf0b48d0107eecc7ba8e4302616ec9
BrianARuff <thelonewolf198927@gmail.com>
2018-11-03 21:16:15 -0400

    .center function

--- 04_string_methods.py
+++ 04_string_methods.py
@@ -13,3 +13,7 @@ print(l_just_string)
 # Justify string on right side with the total amount of white space in string and then print it out.
 r_just_string = stripped_string.rjust(len(string), '.')
 print(r_just_string)
+
+# Center string with nth amount of padding and print it.
+center_pad_string = stripped_string.center(100, '-')
+print(center_pad_string)
